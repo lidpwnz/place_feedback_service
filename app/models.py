3609e66b04cc62a7370be3aa192804cca9236fd8
davidlee <lidpwnz@gmail.com>
2021-10-02 11:25:00 +0600

    #1 Описал модели товара, продукта и категории

--- app/models.py
+++ app/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=150)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=150)
+    category = models.ForeignKey('app.Category', on_delete=models.RESTRICT, related_name='products')
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to=settings.AVATAR_FOLDER, null=True, blank=True)
+
+
+class Feedback(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.SET_DEFAULT, default='AnonymousUser', related_name='feedbacks')
+    product = models.ForeignKey('app.Product', on_delete=models.CASCADE, related_name='feedbacks')
+    description = models.TextField()
+    rate = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
--- core/settings.py
+++ core/settings.py
@@ -126,6 +126,8 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/uploads/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+AVATAR_FOLDER = 'user_avatars'
+AVATAR_DEFAULT_VALUE = ''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
