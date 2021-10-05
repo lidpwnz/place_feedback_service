929dcc0e94a0a57e80f14796401e6cde047ffe9e
davidlee <lidpwnz@gmail.com>
2021-10-05 13:27:37 +0600

    Отрефакторил код, исправил с некорректным отображением формы с ошибками

--- app/helpers.py
+++ app/helpers.py
@@ -1,9 +1,34 @@
+from abc import abstractmethod
+from typing import Any
 from django.urls import reverse_lazy
+from django.views import View
+from accounts.helpers import is_moderator
 from app.forms import ProductForm, FeedbackForm
 from app.models import Product, Feedback
 
 
-class ProductAttrsMixin:
+class BaseAttrsMixin(View):
+    model: Any
+    form_class: Any
+    context_object_name: str
+    template_name: str
+    is_moder: bool
+    object: None
+    permission: str
+
+    def dispatch(self, request, *args, **kwargs):
+        self.is_moder = is_moderator(self.request.user)
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(is_moderator=self.is_moder, **kwargs)
+
+    @abstractmethod
+    def get_success_url(self):
+        return NotImplementedError
+
+
+class ProductAttrsMixin(BaseAttrsMixin):
     model = Product
     form_class = ProductForm
     context_object_name = 'product'
@@ -13,10 +38,12 @@ class ProductAttrsMixin:
         return reverse_lazy('list_product')
 
 
-class FeedbackAttrsMixin:
+class FeedbackAttrsMixin(BaseAttrsMixin):
     model = Feedback
     form_class = FeedbackForm
-    object = None
+
+    def test_func(self):
+        return self.request.user == self.get_object().author or self.request.user.has_perm(self.permission)
 
     def get_success_url(self):
         return reverse_lazy('detail_product', kwargs={'pk': self.object.product.pk})
--- core/signals.py
+++ core/signals.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from app.models import Feedback
@@ -7,12 +6,8 @@ from core.helpers import get_avg
 
 @receiver(signal=post_save, sender=Feedback)
 def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        product = instance.product
+    product = instance.product
+    product_feedbacks = product.feedbacks.filter(is_moderated=True).values_list('rate', flat=True)
 
-        product_feedbacks = product.feedbacks.filter(is_moderated=True).values_list('rate', flat=True)
-        product.avg = get_avg(product_feedbacks)
-        print(get_avg(product_feedbacks))
-
-        product.save()
-        print('Average changed')
+    product.avg = get_avg(product_feedbacks)
+    product.save()
